08903c4c1a3afa2399308953132e0a39acf9dd50
David Martin <home@sirrio.de>
2022-06-22 01:58:24 +0200

    some margins

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -14,12 +14,12 @@ defineProps({
     <Head title="Hochzeit"/>
 
     <div class="absolute w-screen h-screen flex justify-center items-center flex-col">
-        <div class="flex gap-3 text-5xl font-bold text-slate-700">
+        <div class="flex gap-3 text-3xl sm:text-5xl font-bold text-slate-700">
             <div class="-translate-y-5">Shannon</div>
             <div>&</div>
             <div class="translate-y-5">David</div>
         </div>
-        <div class="mt-6 text-3xl text-slate-500">
+        <div class="mt-6 sm:mt-12 text-xl sm:text-3xl text-slate-500">
             ♥ Comming soon ♥
         </div>
     </div>
