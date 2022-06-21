5549f94908b4ed8db84d521e019b3f6b08c3965e
David Martin <home@sirrio.de>
2022-06-21 21:31:21 +0200

    Added coming soon page

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3';
+import bg from '../../images/2395249.jpg'
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
+</script>
+
+<template>
+    <Head title="Hochzeit"/>
+
+    <div class="absolute w-screen h-screen flex justify-center items-center flex-col">
+        <div class="flex gap-3 text-5xl font-bold text-slate-700">
+            <div class="-translate-y-5">Shannon</div>
+            <div>&</div>
+            <div class="translate-y-5">David</div>
+        </div>
+        <div class="mt-6 text-3xl text-slate-500">
+            ♥ Comming soon ♥
+        </div>
+    </div>
+    <img class="w-screen h-screen object-cover" :src="bg" alt="">
+</template>
+
